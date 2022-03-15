83d854c170d2e04ac867dd5950ddb68ed0351c65
rizalzaenal <rizaenalofficial@gmail.com>
2022-03-15 08:48:05 +0700

    init project

---
+++ app/src/main/java/com/rizalzaenal/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.rizalzaenal.weatherapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rizalzaenal.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
