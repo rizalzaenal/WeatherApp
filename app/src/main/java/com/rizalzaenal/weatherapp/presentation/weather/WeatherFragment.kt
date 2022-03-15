341889a9d64c98839286b5907f77c430e8bc29bf
rizalzaenal <rizaenalofficial@gmail.com>
2022-03-15 08:49:59 +0700

    add weather fragment

---
+++ app/src/main/java/com/rizalzaenal/weatherapp/presentation/weather/WeatherFragment.kt
@@ -0,0 +1,10 @@
+package com.rizalzaenal.weatherapp.presentation.weather
+
+import androidx.fragment.app.Fragment
+import com.rizalzaenal.weatherapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WeatherFragment: Fragment(R.layout.fragment_weather) {
+
+}
